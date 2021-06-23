df79fa11acaaab92b1b4edc22690ccfa606f15fd
ryanzhao <ryanzhaors@qq.com>
2021-06-23 12:32:20 +1000

    get data from api

--- CoreData Sample/Utils/API.swift
+++ CoreData Sample/Utils/API.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import PromiseKit
+import Alamofire
+import SwiftyJSON
+
+public class API{
+    
+    // MARK: Get Data
+    public static func getData() -> Promise<JSON> {
+        let url = "https://my-json-server.typicode.com/butterfly-systems/sample-data/purchase_orders"
+        let (promise, seal) = Promise<JSON>.pending()
+        AF.request(url)
+            .validate(statusCode: 200..<300)
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    seal.fulfill(json)
+                case .failure(let error):
+                    seal.reject(error)
+                }
+            }
+        return promise
+    }
+    
+}
+
+
